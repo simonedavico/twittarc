dfc283368ba219c32dbe319f2715f2587aac83cb
Simone D'Avico <simone.davico@welld.ch>
2021-02-03 20:15:20 +0100

    chore: configure typescript

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,10 +1,11 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
-exports.handler = async function http (req) {
+export async function handler(req: unknown) {
   return {
     statusCode: 200,
     headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+      "content-type": "text/html; charset=utf8",
     },
     body: `
 <!DOCTYPE html>
@@ -72,6 +73,6 @@ exports.handler = async function http (req) {
   </div>
 </body>
 </html>
-`
-  }
+`,
+  };
 }
